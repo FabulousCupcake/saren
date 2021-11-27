66c61301358b61f523e73ffbaf00093e5748846d
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-11-27 19:59:07 +0100

    Add simple hanging bot

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, Intents } = require('discord.js');
+
+const TOKEN = process.env.DISCORD_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
