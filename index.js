17ac493463f8c213857154188628088c7ee64b37
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-11-28 13:46:28 +0100

    Add guild members intent

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const TOKEN = process.env.DISCORD_TOKEN;
 // const CLIENT_ID = process.env.CLIENT_ID;
 // const GUILD_ID = process.env.GUILD_ID;
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
 const redisClient = createClient(process.env.REDIS_URL);
 
 const readyHandler = () => {
