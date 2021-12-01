7f79ca7b440fec84023a29efc5b3f7ee993690c5
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-12-01 23:12:25 +0100

    redis: debug

--- pkg/redis/redis.js
+++ pkg/redis/redis.js
@@ -3,6 +3,8 @@ const { createClient } = require("redis");
 let redisClient;
 
 const initializeRedisClient = async () => {
+    console.log("!!!!!!!!!!");
+    console.log(process.env.REDIS_URL);
     redisClient = createClient(process.env.REDIS_URL);
     await redisClient.connect();
     console.log("Successfully initialized Redis Client", redis.INFO("clients"));
