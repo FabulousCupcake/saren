f3e3c3e117b1149a8731c1c25ec5d3bd3c2a8c05
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-12-01 23:13:22 +0100

    redis: specify url correctly

--- pkg/redis/redis.js
+++ pkg/redis/redis.js
@@ -3,9 +3,7 @@ const { createClient } = require("redis");
 let redisClient;
 
 const initializeRedisClient = async () => {
-    console.log("!!!!!!!!!!");
-    console.log(process.env.REDIS_URL);
-    redisClient = createClient(process.env.REDIS_URL);
+    redisClient = createClient({ url: process.env.REDIS_URL });
     await redisClient.connect();
     console.log("Successfully initialized Redis Client", redis.INFO("clients"));
 }
