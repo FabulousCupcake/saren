6ed459d95a918ac4228a631a799b2316bc2fe8df
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-12-02 01:12:42 +0100

    redis: use redis@v3

--- pkg/redis/redis.js
+++ pkg/redis/redis.js
@@ -1,22 +1,51 @@
 const { createClient } = require("redis");
+const { promisify } = require("util");
 
 let redisClient;
+let asyncGet;
+let asyncSet;
+
 
 const initializeRedisClient = async () => {
-    redisClient = createClient({ url: process.env.REDIS_URL });
-    redisClient.on("error", err => console.log('Redis Client Error', err));
-    await redisClient.connect();
+    redisClient = createClient({
+        url: process.env.REDIS_URL,
+        retry_strategy: function(options) {
+            if (options.error && options.error.code === "ECONNREFUSED") {
+                // End reconnecting on a specific error and flush all commands with
+                // a individual error
+                return new Error("The server refused the connection");
+            }
+            if (options.total_retry_time > 1000 * 60 * 60) {
+                // End reconnecting after a specific timeout and flush all commands
+                // with a individual error
+                return new Error("Retry time exhausted");
+            }
+            if (options.attempt > 10) {
+                // End reconnecting with built in error
+                return undefined;
+            }
+            // reconnect after
+            return Math.min(options.attempt * 100, 3000);
+        },
+
+    });
+    // redisClient.on("error", err => console.log('Redis Client Error', err));
+    // await redisClient.connect(); // only needed in redis@v4
+
+    asyncGet = promisify(client.get).bind(client);
+    asyncSet = promisify(client.set).bind(client);
+
     console.log("Successfully initialized Redis Client");
 }
 
 const setArmoryText = async (id, text) => {
     const key = `armory-${id}`;
-    await redisClient.set(key, text);
+    await asyncSet(key, text);
 }
 
 const getArmoryText = async (id) => {
     const key = `armory-${id}`;
-    return await redisClient.get(key);
+    return await asyncGet(key);
 }
 
 module.exports = {
