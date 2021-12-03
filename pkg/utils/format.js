3358d7d69f62d714bcbac32d938927815d8e58b8
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-12-04 00:17:09 +0100

    utils: Add relative time formatter

---
+++ pkg/utils/format.js
@@ -0,0 +1,21 @@
+const units = [
+    ['year', 31536000000],
+    ['month', 2628000000],
+    ['day', 86400000],
+    ['hour', 3600000],
+    ['minute', 60000],
+    ['second', 1000],
+]
+
+const rtf = new Intl.RelativeTimeFormat('en', { style:'narrow'});
+const relatime = elapsed => {
+    for (const [unit, amount] of units) {
+        if (Math.abs(elapsed) > amount || unit === 'second') {
+            return rtf.format(Math.round(elapsed/amount), unit)
+        }
+    }
+}
+
+module.exports = {
+    relatime,
+};
