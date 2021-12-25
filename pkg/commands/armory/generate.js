bcaf48d51787352f09f47de29c6fad3a25d56848
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2021-12-25 12:53:52 +0100

    armory generate: Try generate short url

--- pkg/commands/armory/generate.js
+++ pkg/commands/armory/generate.js
@@ -1,4 +1,5 @@
-const { MessageEmbed } = require("discord.js");
+const https = require('https');
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 const { SlashCommandSubcommandBuilder } = require("@discordjs/builders");
 
 const { isCalledByOwner, isCalledByClanMember } = require("../../acl/acl.js");
@@ -26,6 +27,46 @@ const checkPermissions = interaction => {
         reason: "Can be called by any clan member against anyone"
     }
 };
+
+const generateShortURL = armoryText => {
+    const generateUUID = () => {
+        let uuid = "";
+        while (uuid.length < 8) {
+            digit = parseInt(Math.random() * 16, 10).toString(16);
+            uuid += digit;
+        }
+        return uuid;
+    };
+    const uuid = generateUUID();
+
+    const payload = JSON.stringify({
+        teamList: armoryText,
+        uuid: uuid,
+    });
+
+    const options = {
+        hostname: "pcredivewiki.tw",
+        port: 443,
+        path: "/static/php/mysqlAdd.php",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": payload.length,
+        },
+        timeout: 3000,
+    };
+
+    const promise = new Promise((resolve, reject) => {
+        const req = https.request(options, res => {
+            res.on("end", () => resolve(`https://pcredivewiki.tw/Armory?s=${uuid}`));
+        });
+        req.on("error", () => reject(""));
+        req.write(payload);
+        req.end();
+    });
+
+    return promise;
+}
 
 const generateArmoryTextFunc = async (interaction) => {
     const { allowed, reason } = checkPermissions(interaction);
@@ -56,16 +97,32 @@ const generateArmoryTextFunc = async (interaction) => {
     const armoryTargetText = await getArmoryText(targetUser.id);
     const armoryText = transformToArmorySerializationText(responseBody, armoryTargetText);
 
-    // Reply
+    // Try to make short URL
+    const shortURL = await generateShortURL(armoryText);
+
+    // Build embed
     const messageEmbed = new MessageEmbed()
         .setAuthor(targetUser.username, targetUser.avatarURL())
         .setDescription(armoryText)
         .setColor("F55291")
-        .setFooter(`Generated with data from ${timestamp}`)
+        .setFooter(`Generated with data from ${timestamp}`);
 
+    // Build message commponent
+    const components = [];
+    if (shortURL) {
+        const component = new MessageButton()
+            .setURL(shortURL)
+            .setLabel('Open in Armory')
+            .setStyle('LINK')
+        const componentRow = new MessageActionRow().addComponents(component);
+        components.push(componentRow)
+    }
+
+    // Send reply
     interaction.followUp({
         content: "Here you go!",
         embeds: [ messageEmbed ],
+        components: components,
         ephemeral: true,
     });
 }
