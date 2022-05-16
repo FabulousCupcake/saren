57e25e3551315b026c0dc2ccfca3f051b9563592
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2022-05-16 21:14:43 +0200

    config: add vermilion

--- pkg/config/config.js
+++ pkg/config/config.js
@@ -12,9 +12,17 @@ const VIRIDIAN = {
     spreadsheetId: "1nwXQZyGopqAsYi9uUZkDfOO9RcZtZe_wxsauWVd2hHY",
 };
 
+const VERMILION = {
+    name: "Vermilion",
+    adminRoleId: "891556324750549042",
+    memberRoleId: "891554657095258143",
+    spreadsheetId: "1lcAmLWOPxLsUf42wpFDwGON5opKgZ5-rUEshOz43UGY",
+};
+
 const clanConfigs = [
     VANILLA,
     VIRIDIAN,
+    VERMILION,
 ];
 
 const ownerDiscordId = "109729829670154240";
