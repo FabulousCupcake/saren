91470416f06f69b17f4f635f172cf83229be71bf
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2022-10-01 21:09:36 +0200

    Revert "Remove viridian"
    This reverts commit ce65cd3a8ea1a5b5af1935ba74f4b74885e96dd3.

--- pkg/config/config.js
+++ pkg/config/config.js
@@ -5,6 +5,13 @@ const VANILLA = {
     spreadsheetId: "1tpbLmB4Fha_0TpMd4h3d2xmpf_GztvpFP_DFmdaOHfM",
 };
 
+const VIRIDIAN = {
+    name: "Viridian",
+    adminRoleId: "923091835667234847",
+    memberRoleId: "923090620019535902",
+    spreadsheetId: "1nwXQZyGopqAsYi9uUZkDfOO9RcZtZe_wxsauWVd2hHY",
+};
+
 const VVEEPO = {
     name: "VVeepo",
     adminRoleId: "947923764832792616",
@@ -14,6 +21,7 @@ const VVEEPO = {
 
 const clanConfigs = [
     VANILLA,
+    VIRIDIAN,
     VVEEPO,
 ];
 
