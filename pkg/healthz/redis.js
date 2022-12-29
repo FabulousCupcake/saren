07ba62b1549b7799baa89a2720dbb77ed215aed7
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2022-12-29 17:25:18 +0100

    healthz: debug

--- pkg/healthz/redis.js
+++ pkg/healthz/redis.js
@@ -3,6 +3,9 @@ const { getRedisClient } = require("../redis/redis");
 const checkRedisClient = async () => {
   const redisClient = getRedisClient();
 
+  console.log("isOpen", redisClient.isOpen);
+  console.log("isReady", redisClient.isReady);
+
   if (!redisClient.isOpen) return false;
   if (!redisClient.isReady) return false;
 
